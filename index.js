032eb812fc15d6d86c3429d74a46b121a1ca3bb0
srs-sieam <srssieam@gmail.com>
2023-11-12 06:05:55 +0600

    created a collection for reviews and sent to client

--- index.js
+++ index.js
@@ -29,11 +29,19 @@ async function run() {
 
 
         const menuCollection = client.db("bistroDB").collection("menu");
+        const reviewsCollection = client.db("bistroDB").collection("reviews");
 
         app.get('/menu', async(req, res) =>{
             const result = await menuCollection.find().toArray();
             res.send(result);
         })
+
+
+        app.get('/reviews', async(req, res) =>{
+            const result = await reviewsCollection.find().toArray();
+            res.send(result);
+        })
+
 
 
         // Send a ping to confirm a successful connection
