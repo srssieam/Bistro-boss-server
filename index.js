781b1c3fa0941266f0dbef79a61d95629be1b81b
srs-sieam <srssieam@gmail.com>
2023-11-14 09:44:23 +0600

    get cart items from database and send to client

--- index.js
+++ index.js
@@ -50,6 +50,11 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/carts', async(req, res) =>{
+            const result = await cartCollection.find().toArray();
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
