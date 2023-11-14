9885419876f8daa098d0d3632de632761c228317
srs-sieam <srssieam@gmail.com>
2023-11-14 08:55:29 +0600

    receive cart item from client and store into data base

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
 
         const menuCollection = client.db("bistroDB").collection("menu");
         const reviewsCollection = client.db("bistroDB").collection("reviews");
+        const cartCollection = client.db("bistroDB").collection("carts");
 
         app.get('/menu', async(req, res) =>{
             const result = await menuCollection.find().toArray();
@@ -42,6 +43,12 @@ async function run() {
             res.send(result);
         })
 
+        // carts collection
+        app.post('/carts', async(req, res) =>{
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem)
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
