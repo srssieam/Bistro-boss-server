2666d560328e19949e08a682fbab4bcd56448117
srs-sieam <srssieam@gmail.com>
2023-11-16 09:40:11 +0600

    get users from database

--- index.js
+++ index.js
@@ -81,6 +81,12 @@ async function run() {
             res.send(result);
         })
 
+        // get user from db
+        app.get('/users', async(req, res) =>{
+            const result = await userCollection.find().toArray();
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
