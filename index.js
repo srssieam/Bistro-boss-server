e5e48e293ca57929ec8ba7143d8a98ff1d033aaa
srs-sieam <srssieam@gmail.com>
2023-11-16 09:51:16 +0600

    receive user id from client and delete the user from database

--- index.js
+++ index.js
@@ -87,6 +87,14 @@ async function run() {
             res.send(result)
         })
 
+        // delete a user
+        app.delete('/users/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result)
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
