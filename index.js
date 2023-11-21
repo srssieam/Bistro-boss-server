06dc71ebf82dc6d3144afdb28dd6dfe85701fed2
srs-sieam <srssieam@gmail.com>
2023-11-21 08:28:26 +0600

    get data for admin stats

--- index.js
+++ index.js
@@ -241,6 +241,31 @@ async function run() {
             res.send(result);
         })
 
+        // stats or analytics
+        app.get('/admin-stats', verifyToken, verifyAdmin, async(req, res) => {
+            const users = await userCollection.estimatedDocumentCount();
+            const menuItems = await menuCollection.estimatedDocumentCount();
+            const orders = await paymentCollection.estimatedDocumentCount();
+
+            // this is not the best way
+            // const payments = await paymentCollection.find().toArray();
+            // const revenue = payments.reduce((total, payment)=> total + payment.price,0)
+
+            const result = await paymentCollection.aggregate([
+                {
+                    $group: {
+                        _id: null,
+                        totalRevenue: {
+                            $sum: "$price"
+                        }
+                    }
+                }
+            ]).toArray();
+            const revenue = result.length > 0 ? result[0].totalRevenue : 0; 
+
+            res.send({users, menuItems, orders, revenue});
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
